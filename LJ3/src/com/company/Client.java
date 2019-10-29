5b5451c474dfac2f7384d262e0e5f42e16651c9a
HolloWarn <56124403+HolloWarn@users.noreply.github.com>
2019-10-29 22:54:07 +0300

    0.01

--- LJ3/src/com/company/Client.java
+++ LJ3/src/com/company/Client.java
@@ -1,5 +1,4 @@
 package com.company;
 
 public class Client extends User {
-
 }
---
+++ LJ3/src/com/company/Totalizator.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.Scanner;
+import java.util.ArrayList
+
+public class Totalizator {
+
+    public static void main(String[] args) {
+        List horses = new ArrayList();
+        List users = new ArrayList();
+        User CurrentUser = new user();
+        Scanner in = new Scanner(System.in);
+        int OK = 1;
+	    while (OK == 1)
+        {
+            System.out.println("1.Зарегистрироваться\n" +
+                               "2.Войти\n" +
+                               "0.Выход\n");
+            int number = in.nextInt();
+            switch(number) {
+            case 1:
+
+                break;
+            case 2:
+
+            break;
+            case 0:
+                OK = 0;
+                break;
+    }
+    }
+    }
+    private void addUser()
+    {}
+    private void
+    }
+
